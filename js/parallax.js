11b95ad70a7433e2c9c2e83d951796d33461b52f
Pato <pj.tabilo@gmail.com>
2023-03-19 01:49:19 -0300

    feat: parallax en los personajes

--- js/parallax.js
+++ js/parallax.js
@@ -1,15 +1,17 @@
-const image = document.querySelector('.construction-image');
-const container = document.querySelector('.container');
+const personajes = document.querySelector('.characters');
+const logo = document.querySelector('.logo');
+const texto = document.querySelector('.coming-soon-text');
+const contenedor = document.querySelector('body');
 
-container.addEventListener('mousemove', (event) => {
+contenedor.addEventListener("mousemove", function(e) {
   const mouseX = event.clientX;
   const mouseY = event.clientY;
 
-  const centerX = container.clientWidth / 2;
-  const centerY = container.clientHeight / 2;
+  const centerX = contenedor.clientWidth / 2;
+  const centerY = contenedor.clientHeight / 2;
 
   const offsetX = (mouseX - centerX) / 50;
   const offsetY = (mouseY - centerY) / 50;
 
-  image.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  personajes.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 });
